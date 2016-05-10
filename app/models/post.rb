75c92819befa6233358fa89e629b294dc53b1736
TheCrott <aldothecrott@gmail.com>
2016-05-10 22:57:00 +0700

    create destroy function in comments controller
    can destroy comment, and make comments depend to post (on delete
    cascade)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true, length: { minimum: 10 }
   validates :body, presence: true
-
-  has_many :comments
 end
